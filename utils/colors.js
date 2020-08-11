2ccdf83ecbd002a151393664e55be2a780ead332
robhhr <robhhr@outlook.com>
2020-08-10 19:03:24 -0700

    update color schemes

--- utils/colors.js
+++ utils/colors.js
@@ -17,11 +17,35 @@ const SOLARIZED_COLORS = [
   {paletteName: 'Green', colors: '#859900'},
 ]
 
+const CRYSTAL_SEA = [
+  {paletteName: 'Soft Coral', colors: '#85d2b9'},
+  {paletteName: 'Green Aqua', colors: '#5d9c94'},
+  {paletteName: 'Nature Green', colors: '#3b898b'},
+  {paletteName: 'Elegant Blue', colors: '#447a94'},
+  {paletteName: 'Softened Royal', colors: '#3c6081'},
+]
+
 const ORANGE_CREME = [
   {paletteName: 'Vanilla', colors: '#fdf2b8'},
   {paletteName: 'Orange', colors: '#e88200'},
   {paletteName: 'Burnt Orange', colors: '#cb2800'},
   {paletteName: 'Coffee', colors: '#34262b'},
+]
+
+const GOLDEN_LAVENDER = [
+  {paletteName: 'Royal Gold', colors: '#a97218'},
+  {paletteName: 'Bright Currency', colors: '#e0b34a'},
+  {paletteName: 'Serious Tempo', colors: '#77526b'},
+  {paletteName: 'Spring Memories', colors: '#a26499'},
+  {paletteName: 'Bright Fate', colors: '#e6a4ff'},
+]
+
+const MUTED_SPLASH = [
+  {paletteName: 'Saturated Yellow', colors: '#d5d609'},
+  {paletteName: 'Prominent Pink', colors: '#dd2164'},
+  {paletteName: 'Nature Heritage', colors: '#079041'},
+  {paletteName: 'Dark Green Aqua', colors: '#1c817e'},
+  {paletteName: 'Colorful Splash', colors: '#6e04a9'},
 ]
 
 const WILD_JUNGLE = [
@@ -38,18 +62,39 @@ const SUMMER_TONES = [
   {paletteName: 'Yellow Mustard', colors: '#a4c200'},
 ]
 
+const CRAFTSMAN_COLLECTION = [
+  {paletteName: 'Soft Cream', colors: '#d7c797'},
+  {paletteName: 'Royal Wood', colors: '#845422'},
+  {paletteName: 'Bright Accent', colors: '#ead61c'},
+  {paletteName: 'Classic Wood', colors: '#a47c48'},
+  {paletteName: 'Dark Accent', colors: '#000000'},
+]
+
+const STRAWBERRY_RAIN = [
+  {paletteName: 'Soft Red', colors: '#ff7474'},
+  {paletteName: 'Cotton Candy', colors: '#f695a4'},
+  {paletteName: 'Happy Green', colors: '#cff4df'},
+  {paletteName: 'Coral King', colors: '#abdfd6'},
+  {paletteName: 'Calm Waves', colors: '#a4cddd'},
+]
+
 const DISCORD_COLORS = [
   {paletteName: 'Digital Purple', colors: '#7289da'},
-  {paletteName: 'White', colors: '#ffffff'},
-  {paletteName: 'Saturn Pink', colors: '#99aab5'},
-  {paletteName: 'Dark Coffee', colors: '#2c2f33'},
-  {paletteName: 'Real Oak', colors: '#23272a'},
+  {paletteName: 'Serious Etiquette', colors: '#424549'},
+  {paletteName: 'Elegant Night', colors: '#36393e'},
+  {paletteName: 'Classic Curtain', colors: '#282b30'},
+  {paletteName: 'Real Night', colors: '#1e2124'},
 ]
 
 const COLOR_PALETTES = [
   {paletteName: 'Solarized', colors: SOLARIZED_COLORS},
+  {paletteName: 'Crystal Sea', colors: CRYSTAL_SEA},
+  {paletteName: 'Muted Splash', colors: MUTED_SPLASH},
   {paletteName: 'Orange Creme', colors: ORANGE_CREME},
+  {paletteName: 'Golden Lavender', colors: GOLDEN_LAVENDER},
+  {paletteName: 'Craftsman Collection', colors: CRAFTSMAN_COLLECTION},
   {paletteName: 'Wild Jungle', colors: WILD_JUNGLE},
+  {paletteName: 'Strawberry In The Rain', colors: STRAWBERRY_RAIN},
   {paletteName: 'Summer Tones', colors: SUMMER_TONES},
   {paletteName: 'Discord', colors: DISCORD_COLORS},
 ]
