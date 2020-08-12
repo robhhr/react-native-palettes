fb9fc57ba84d7705c734089e05a676269e34b32b
robhhr <robhhr@outlook.com>
2020-08-11 23:03:56 -0700

    create palette inputs

--- components/ColorPaletteModal.js
+++ components/ColorPaletteModal.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import {Text, View} from 'react-native'
+import React, {useState} from 'react'
+import {StyleSheet, Text, TextInput, View} from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 25,
+    paddingTop: 20,
+  },
+  input: {
+    borderColor: '#b7b7b7',
+    borderWidth: 1,
+    marginTop: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    height: 35,
+  },
+})
 
 const ColorPaletteModal = () => {
+  const [paletteName, setPaletteName] = useState('')
   return (
-    <View>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <Text>Name</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPaletteName}
+        placeholder={'My first color palette'}
+        value={paletteName}
+        clearButtonMode={'while-editing'}
+        clearTextOnFocus={true}
+        enablesReturnKeyAutomatically={true}
+      />
     </View>
   )
 }
